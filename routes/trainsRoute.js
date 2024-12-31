ed9e36030539f9ffdf64ea18c1dfcc7213f4f3a8
sreenivasulu-sdlglobe <sreenivasulu.g@sdlglobe.com>
2024-12-31 18:15:24 +0530

    added train fare available API from IRCTC

--- routes/trainsRoute.js
+++ routes/trainsRoute.js
@@ -4,6 +4,7 @@ const trainController = require('../controllers/train')
 const isAuth = require("../middleware/isAuth")
 
 router.get('/getStation', trainController.getStation);
-router.post('/getTrains',trainController.getTrains);
+router.post('/getTrains', trainController.getTrains);
+router.post('/getTrains/avlFareEnquiry',isAuth.token, trainController.getTrainsAvailableFareEnquiry);
 
 module.exports = router;
