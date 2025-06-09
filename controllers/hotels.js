d2c065c44a2bc322bab4d703438f66a55b13b458
sreenivasulu-sdlglobe <sreenivasulu.g@sdlglobe.com>
2025-06-09 17:49:54 +0530

    Update hotel API to use environment variables for credentials and rename getHotels method to getHotelsList

--- controllers/hotels.js
+++ controllers/hotels.js
@@ -7,10 +7,10 @@ const base_url = 'https://sandboxentityapi.trateq.com/';
 const credentials =  {
     "Type": "C",
     "Module": "X",
-    "Domain": "SDB",
-    "LoginID": "INDSNI00",
-    "Password": "Mallik@123",
-    "LanguageLocale": "en-US",
+    "Domain": process.env.DOMAIN,
+    "LoginID": process.env.LOGIN_ID,
+    "Password": process.env.PASSWORD,
+    "LanguageLocale": process.env.LANGUAGE,
     "IpAddress": "8.8.8.8"
 }
 
@@ -32,18 +32,24 @@ exports.getHotelCities = async (req, res) => {
     }
 };
 
-exports.getHotels = async (req, res) => {
+exports.getHotelsList = async (req, res) => {
     const { cityId, checkInDate, checkOutDate, Rooms } = req.body;
-
+    
+    if (!cityId || !checkInDate || !checkOutDate || !Rooms) {
+        return res.status(400).json({ error: 'Missing required fields' });
+    }
+    
     try {
         const response = await axios.post(`${base_url}/SIGNIX/B2B/Hotel/CacheSearch`, {
             "Credential": credentials,
             "CityId": cityId,
-            "CheckInDate": moment(checkInDate).format('YYYY-MM-DD'),
-            "CheckOutDate": moment(checkOutDate).format('YYYY-MM-DD'),
+            "CheckInDate": checkInDate,
+            "CheckOutDate": checkOutDate,
+            "Currency": "INR",
+            "showDetail": false,
             "Rooms": Rooms
         });
-
+        console.log("Response:", response.data);
         res.status(200).json(response.data);
     } catch (error) {
         console.error('Error fetching hotels:', error.message);
