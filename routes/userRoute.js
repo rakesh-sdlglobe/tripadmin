90e1bfe1d55304af7129e5ebb608d7873787f5b1
sarathkumar-19 <sarathkumar.v@sdlglobe.com>
2025-07-08 16:41:19 +0530

    Update dependencies and refactor user authentication logic
    - Added `cloudinary` and `multer-storage-cloudinary` to handle image uploads.
    - Updated `dotenv` to version 16.6.1.
    - Refactored user authentication in `auth.js` to improve token generation and user data handling.
    - Enhanced user profile retrieval and editing in `user.js`, including marital status management.
    - Updated routes to reflect changes in image upload handling.
    - Adjusted middleware to pass user data correctly after token verification.

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user')
 const isAuth = require("../middleware/isAuth")
+const upload = require("../middleware/ImageUpdate.js")
 
 router.get('/getUsers',isAuth.token, userController.getRecentUsers);
 router.get('/userProfile', isAuth.token, userController.getUserProfile)
@@ -10,6 +11,6 @@ router.get('/myBookings', isAuth.token, userController.myBookings)
 router.post('/addTraveler', isAuth.token, userController.addTraveller)
 router.get('/getTravelers', isAuth.token, userController.getTravelers)
 router.delete('/traveller/:id', isAuth.token, userController.removeTraveller);
-router.post('/imageUpload', isAuth.token,userController.imageUpload);
+router.put('/imageUpload/:id', isAuth.token, upload.single('img_url'), userController.imageUpload);
 
 module.exports = router;
