cf9dfe524f65b0d95d6805b64b3411a2b184573a
Rakesh swain <142288684+rakesh-sdlglobe@users.noreply.github.com>
2025-08-16 10:45:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const PORT = 3002;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
