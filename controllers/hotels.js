250928dcf5a7f07d8ee7fc3a84072c8c9cb25e8b
Rakesh swain <142288684+rakesh-sdlglobe@users.noreply.github.com>
2025-08-18 17:22:23 +0530

    Update hotels.js

--- controllers/hotels.js
+++ controllers/hotels.js
@@ -26,19 +26,25 @@ const credentials = {
 exports.getHotelCities = async (req, res) => {
   const { input } = req.body;
   try {
-    const response = await axios.post(`${base_url}/SIGNIX/B2B/StaticData/AC`, {
-      Credential: credentials,
-      AcType: "CityHotel",
-      SearchText: input || "",
-      AllData: input ? true : false,
-    }, {
-  headers: {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
-    "Accept": "application/json, text/plain, */*",
-    "Accept-Language": "en-US,en;q=0.9",
-    "Content-Type": "application/json"
-  }                       
-   );
+    const response = await axios.post(
+  `${base_url}/SIGNIX/B2B/StaticData/AC`,
+  {
+    Credential: credentials,
+    AcType: "CityHotel",
+    SearchText: input || "",
+    AllData: input ? true : false,
+  },
+  {
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+      "Accept": "application/json, text/plain, */*",
+      "Accept-Language": "en-US,en;q=0.9",
+      "Content-Type": "application/json",
+    },
+  }
+);
+
 
     res.status(200).json(response.data);
   } catch (error) {
