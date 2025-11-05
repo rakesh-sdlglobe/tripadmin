257df43eb9ee65e647f48e28cdc6faaacff84f23
sreenivasulu-sdlglobe <sreenivasulu.g@sdlglobe.com>
2025-11-05 15:51:07 +0530

    flight update

--- routes/flightsRoutes.js
+++ routes/flightsRoutes.js
@@ -5,6 +5,11 @@ const isAuth = require("../middleware/isAuth");
 
 router.post("/getFlightsAirports", flightController.getFlightsAirports);
 router.post("/getFlightsList", flightController.getFlightsList);
+router.post("/getFlightFareRule", flightController.getFlightFareRule);
 router.post("/getFlightPriceValidate", flightController.getFlightPriceValidate);
+router.post("/getFlightServiceTax", flightController.getFlightServiceTax);
+router.post("/getFlightPreBook", flightController.getFlightPreBook);
+router.post("/getFlightBook", flightController.getFlightBook);
+router.post("/getFlightBookDetails", flightController.getFlightBookDetails);
 
 module.exports = router;
